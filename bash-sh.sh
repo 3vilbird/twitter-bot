5731e24a0260fa65c3c802eaf90d241ff6556ab0
ganapati marathi <40819456+blackthedevil@users.noreply.github.com>
2020-02-05 07:12:51 -0500

    Delete bash-sh.sh

--- bash-sh.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "give me a bottle of rum!"
-
-git init
-git add .
-git commit -m "first commit"
-git remote add origin https://github.com/blackthedevil/twitter-bot.git
-git push -u origin master
